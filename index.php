353d35d3025bc8ee2fa393308de6f39dd8f2d503
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-04-24 13:35:34 +0700

    create form

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>HELLO</h1>
+
+    <form action="" method="post">
+    <label for="">Masukan Nama</label>
+    <input type="text" name="name"><br>
+
+    <label for="">Masukan Email</label>
+    <input type="email" name="email"><br>
+
+    <label for="">Masukan Pesan</label>
+    <input type="text" name="message"><br>
+
+    <button type="submit" name="submit">Kirim</button>
+
+    </form>
+
+
+
+</body>
+</html>
